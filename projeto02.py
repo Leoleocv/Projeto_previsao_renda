e900a7aaf6a8736d0b4382dc307586f5fb4d7274
Leoleocv <leodecvlho@gmail.com>
2025-03-15 21:12:28 -0300

    teste

--- projeto02.py
+++ projeto02.py
@@ -65,6 +65,20 @@ sns.despine()
 
 st.pyplot(plt)
 
+st.write('## Model Evaluation Metrics')
+st.write(f'MSE (train): 35031681.34')
+st.write(f'R2 (train): 0.4884')
+st.write(f'MSE: 34803648.75')
+st.write(f'R2: 0.4864')
 
+st.write('## Conclusion')
+st.write('A modelagem foi realizada utilizando algoritmos de aprendizado de máquina, sendo avaliada com métricas como Mean Squared Error (MSE) e R². O desempenho do modelo foi satisfatório, conseguindo capturar padrões importantes nos dados. No entanto, algumas limitações foram observadas, como a presença de outliers e possíveis variáveis latentes não disponíveis no conjunto de dados, que poderiam melhorar a previsibilidade do modelo.')
+st.write('Possíveis melhorias incluem o uso de técnicas mais avançadas e a inclusão de mais variáveis que possam impactar a renda, a previsão de renda é uma tarefa complexa, influenciada por múltiplas variaveis. Conseguindo prever cerca de 50% d O modelo desenvolvido pode ser útil para segmentação de clientes, oferta de produtos financeiros e otimização de concessão de crédito, contribuindo para melhores decisões estratégicas no setor bancário.')
 
-
+st.write('## Distribution of Renda')
+fig_hist, ax_hist = plt.subplots()
+sns.histplot(renda["renda"], bins=50, kde=True, ax=ax_hist)
+ax_hist.set_title("Distribution of Renda")
+ax_hist.set_xlabel("Renda")
+ax_hist.set_ylabel("Frequência")
+st.pyplot(fig_hist)
